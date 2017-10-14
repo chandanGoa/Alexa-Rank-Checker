5438e08043e3bf4609785efb8e454d6b66c32f7e
Chandan Dsa <chandan.dsa@gmail.com>
2017-10-14 05:36:50 +0530

    Add files via upload

---
+++ alexa_helper.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+* @copyright © 2017 WeaveBox.in
+*/
+
+function alexaRank($site)
+{
+    $xml = simplexml_load_file('http://data.alexa.com/data?cli=10&dat=snbamz&url=' .
+        $site);
+
+    $a = $xml->SD[1]->POPULARITY;
+    if ($a != null)
+    {
+        $alexa_rank = $xml->SD[1]->POPULARITY->attributes()->TEXT;
+        $alexa_rank = ($alexa_rank==null ? 'No Global Rank' : $alexa_rank);
+    } else
+    {
+        $alexa_rank = 'No Global Rank';
+    }
+	   
+    $a1 = $xml->SD[1]->COUNTRY;
+
+    if ($a1 != null)
+    {
+        $alexa_pop = $xml->SD[1]->COUNTRY->attributes()->NAME;
+        $regional_rank = $xml->SD[1]->COUNTRY->attributes()->RANK;
+        $alexa_pop = ($alexa_pop==null ? 'None' : $alexa_pop);
+        $regional_rank = ($regional_rank==null ? 'None' : $regional_rank);
+
+    } else
+    {
+        $alexa_pop = 'None';
+        $regional_rank = 'None';
+    }
+	
+	$a3 = $xml->RLS->RL;
+	if ($a3 != null)
+    {
+		$related = array();
+		$i = 0;
+		foreach($a3 as $ap){
+			$related[$i]['title'] = (string)$ap->attributes()->TITLE;
+			$related[$i]['href'] = (string)$ap->attributes()->HREF;
+			$i++;
+		}
+
+    } else
+    {
+        $related[$i]['title'] = 'None';
+        $related[$i]['href'] = 'None';
+    }
+
+    $a2 = $xml->SD[0]->LINKSIN;
+    if ($a2 != null)
+    {
+        $alexa_back = $xml->SD[0]->LINKSIN->attributes()->NUM;
+		/*
+        if ($alexa_back==null)
+        {
+            $outData = getMyData("http://www.alexa.com/siteinfo/$site");
+            $back = explode('<span class="font-4 box1-r">',$outData);
+            $back = explode('</span>',$back[1]);
+            $alexa_back = $back[0];
+        }
+		*/
+    } else
+    {
+        $alexa_back = '0';
+    }
+    $alexa_back = ($alexa_back==null ? '0' : $alexa_back);
+    return array($alexa_rank,$alexa_pop,$regional_rank,$alexa_back);
+}
+
+?>
